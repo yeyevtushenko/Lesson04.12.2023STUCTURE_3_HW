926d307cc26d62e10c24d07ed8502cb6fcf40f9c
yeyevtushenko <yehor803@gmail.com>
2024-01-03 15:34:25 +0200

    Ініціалізуємо інший клас для зупинки катерів

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ class Passenger:
     def __init__(self, arrival_time, stay_time):
         self.arrival_time = arrival_time
         self.stay_time = stay_time
+
+class BoatStop:
+    def __init__(self, arrival_interval, boat_interval, stop_type, max_people):
+        self.arrival_interval = arrival_interval
+        self.boat_interval = boat_interval
+        self.stop_type = stop_type
+        self.max_people = max_people
+        self.people_queue = queue.Queue()
+        self.boats_at_stop = []
+        self.current_time = 0

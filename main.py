9dcc787bc2843e47986098671b0e44a8bd5babd6
yeyevtushenko <yehor803@gmail.com>
2024-01-03 15:33:21 +0200

    Ініціалізуємо клас, що представляє пасажира, який прийшов на очікує на причалі

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import random
 import time
 import queue
+
+class Passenger:
+    def __init__(self, arrival_time, stay_time):
+        self.arrival_time = arrival_time
+        self.stay_time = stay_time
